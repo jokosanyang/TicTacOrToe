11a8494e7febda088147524c8c5d09345af49397
Joko Sanyang <jo.san@live.co.uk>
2019-06-30 21:08:58 +0100

    refactor App to own file, wrote a test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    return (
+		<React.Fragment>
+			<h1>Welcome!</h1>
+			<h2>
+				If you've come to finally find the Truth, you're in the right place.
+			</h2>
+		</React.Fragment>
+	);
+}
+	
+export default App;
