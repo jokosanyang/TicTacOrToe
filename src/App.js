b6a5e46ba3dfd7847f19b5ebb501db096580908b
Joko Sanyang <jo.san@live.co.uk>
2019-06-30 21:51:12 +0100

    Add conditionally rendered question component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import Question from './components/Question/Question';
 
 const App = () => {
-    return (
+	const [started, setStart] = React.useState(null);
+
+	return (
 		<React.Fragment>
-			<h1>Welcome!</h1>
-			<h2>
-				If you've come to finally find the Truth, you're in the right place.
-			</h2>
+			<header>
+				<h1>TIC, TAC, TOE</h1>
+			</header>
+			{started ? (
+				<Question />
+			) : (
+				<div>
+					<h2>
+						If you've come to finally find the Truth, you're in the right place.
+						<br />
+						Answer four quick questions and find out once and for all, if you're
+						a tic, a tac, or a toe.
+					</h2>
+					<button onClick={() => setStart(true)}>Start</button>
+				</div>
+			)}
 		</React.Fragment>
 	);
-}
-	
+};
+
 export default App;
