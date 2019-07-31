2837834d843e386ef4636a27f3d5ad66f7202dfa
Joko Sanyang <jo.san@live.co.uk>
2019-07-31 16:46:27 +0000

    Fix test and move to app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import Question from './components/Question/Question';
 
 test("App renders in the Dom", () => {
     const div = document.createElement('div');
     ReactDOM.render(<App/>, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+test('clicking first answer increments tic variable', () => {
+	const { getByText } = render(<Question number={1} numberFunction={(number)=> number+1 }/>);
+	const Button1 = getByText(/dayummm/);
+	fireEvent.click(Button1);
+    
 });
