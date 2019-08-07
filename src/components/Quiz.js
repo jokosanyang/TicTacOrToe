7c9a2f4c46bcb060da1ea3079364df4d314bd716
Joko Sanyang <jo.san@live.co.uk>
2019-08-07 16:12:47 +0100

    Modularise out the component

---
+++ src/components/Quiz.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Question from './Question/Question';
+import Result from './Result/Result';
+
+const Quiz = (props) => {
+
+	const [questionNumber, incrementNumber] = React.useState(1);
+	const [tic, setTic] = React.useState(0);
+	const [tac, setTac] = React.useState(0);
+	const [toe, setToe] = React.useState(0);
+
+	return (
+		<React.Fragment>
+			{questionNumber > 4 ? (
+				<Result tic={tic} tac={tac} toe={toe}/>
+			) : (
+				<Question number={questionNumber} numberFunction={incrementNumber} tic={tic} setTic={setTic} tac={tac} setTac={setTac} toe={toe} setToe={setToe} />
+			)}
+		</React.Fragment>
+	);
+};
+
+export default Quiz;
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Result = (props) => {
+	const { tic, tac, toe } = props;
+	const tictactoe = [tic, tac, toe];
+	console.log(tictactoe);
+
 	return (
 		<section>
 			<h2>Wow! You're a bleep!</h2>
