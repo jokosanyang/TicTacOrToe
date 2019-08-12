6d51363eedf231f36c832cbce7f462f5a9da7aea
Joko Sanyang <jo.san@live.co.uk>
2019-08-12 21:50:19 +0100

    Add Twitter link and change images

--- src/App.style.js
+++ src/App.style.js
@@ -1,24 +1,5 @@
 import styled from 'styled-components';
 
-const Answer = styled.button`
-	border: none;
-	font-family: inherit;
-	font-size: inherit;
-	color: inherit;
-	cursor: pointer;
-	display: inline-block;
-	margin: 1rem 1rem;
-	text-transform: uppercase;
-	letter-spacing: 1px;
-	font-weight: 700;
-	outline: none;
-	position: relative;
-	transition: all 0.3s;
-	background: #fff;
-	box-shadow: 0 5px;
-	padding: 25px 60px 25px 60px;
-`;
-
 const Button = styled.button`
 	border: none;
 	font-family: inherit;
@@ -38,6 +19,32 @@ const Button = styled.button`
 	padding: 1rem 2rem;
 `;
 
+const Answer = styled(Button)`
+	color: #000;
+	background: #fff;
+	padding: 25px 60px 25px 60px;
+`;
+
+const Link = styled.a`
+	border: none;
+	font-family: inherit;
+	font-size: inherit;
+	cursor: pointer;
+	display: inline-block;
+	margin: 1rem 1rem;
+	text-transform: uppercase;
+	letter-spacing: 1px;
+	font-weight: 700;
+	outline: none;
+	position: relative;
+	transition: all 0.3s;
+	box-shadow: 0 5px;
+	padding: 1rem 2rem;
+	color: #000;
+	background: #fff;
+	padding: 25px 60px 25px 60px;
+`;
+
 const Image = styled.img`
 	width: 35%;
 	height: 35%;
@@ -48,4 +55,4 @@ const Container = styled.section`
 	flex-direction: column;
 	align-items: center;
 `;
-export { Answer, Button, Image, Container };
+export { Answer, Button, Image, Container, Link };
